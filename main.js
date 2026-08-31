b101591be0504e8c03069ec1b6bff609bd39596b
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-08-31 14:01:28 +0300

    Stage L: wire share-readiness UI

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ import './src/package8TeacherUi.css'
 import './src/stageATeacherPresentation.css'
 import './src/package11TunerUi.css'
 import './src/stageKTunerPresentation.css'
+import './src/stageLShareUi.css'
 import './src/appShell.css'
 import './src/mobileReviewUi.css'
 import './src/stageDQualityOverlay.css'
@@ -33,9 +34,11 @@ import './src/reviewInspectorUi.js'
 import { initStageIInstrumentProductUi } from './src/stageIInstrumentProductUi.js'
 import { initStageJDiscoveryPresentation } from './src/stageJDiscoveryPresentation.js'
 import { initStageKTunerPresentation } from './src/stageKTunerPresentation.js'
+import { initStageLShareUi } from './src/stageLShareUi.js'
 
 if (typeof document !== 'undefined') {
   initStageIInstrumentProductUi(document)
   initStageJDiscoveryPresentation(document)
   initStageKTunerPresentation(document)
+  initStageLShareUi(document)
 }
