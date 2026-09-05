b294a3112a3eb64fb718692aa7e8f4d4ef879c97
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-05 16:55:59 +0300

    Fix Express 5 static server route

--- experiments/smoosic-mobile/server.js
+++ experiments/smoosic-mobile/server.js
@@ -6,7 +6,6 @@ const publicDir = path.join(__dirname, 'public');
 const port = process.env.PORT || 10000;
 
 app.use(express.static(publicDir));
-app.get('*', (_req, res) => res.sendFile(path.join(publicDir, 'index.html')));
 app.listen(port, '0.0.0.0', () => {
   console.log(`Smoosic mobile POC listening on ${port}`);
 });
